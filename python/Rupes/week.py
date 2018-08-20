80007abf75ac27862bc85db77b334192d30605a3
nixis-institute <nixis.institute@gmail.com>
2018-08-20 16:41:09 +0530

    update

---
+++ python/Rupes/week.py
@@ -0,0 +1,17 @@
+n=(int(input('Enter the value ')))
+if (n==1):
+    print('Today is Monday')
+elif (n==2):
+    print('Today is Tuseday')
+elif (n==3):
+    print('Today is Wednesday')
+elif (n==4):
+    print('Today is Thasday')
+elif (n==5):
+    print('Today is Friday')
+elif (n==6):
+    print('Today is Starday')
+elif (n==7):
+    print('Today is  Sunday')
+
+
