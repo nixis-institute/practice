672d077c24f5ecd79eb68822eeca880c04eccb62
nixis-institute <nixis.institute@gmail.com>
2018-08-30 13:10:03 +0530

    update

---
+++ python/bhawna/prime.py
@@ -0,0 +1,21 @@
+number=int(input("enter any value"))
+
+
+def isprime(number):
+    flag = 0
+    for i in range(2,number):
+        if number%i==0:
+            flag = 1
+            break
+        else:
+            flag = 0
+    if(flag==0):
+        return True
+    else:
+        return False
+
+
+
+for i in range(3,number):
+    if(isprime(i)):
+        print(i)
