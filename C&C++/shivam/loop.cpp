27abb83893586cc27744b7650ac4b3de42422062
himrah <rkrjob55@gmail.com>
2018-10-09 09:25:19 +0530

    update

---
+++ C&C++/shivam/loop.cpp
@@ -0,0 +1,13 @@
+#include "iostream"
+using namespace std;
+int main()
+{
+    int i,sp,ep;
+    cout<<"enter a 1 value ";
+    cin>>sp;
+    cout<<"enter a 2 value ";
+    cin>>ep;
+    for(i=sp;i<=ep;i++)
+    cout<<"\n"<<i;
+return 0;
+}
