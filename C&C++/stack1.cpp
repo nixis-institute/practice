aae02e4ab56b9a8352c5e245deefff9af7dbbe4b
nixis-institute <nixis.institute@gmail.com>
2018-10-16 18:31:39 +0530

    update

---
+++ C&C++/stack1.cpp
@@ -0,0 +1,68 @@
+#include "iostream"
+using namespace std;
+int max=10;
+int top=-1;
+int stack[10];
+void isempty()
+{
+    if(top==-1)
+    {
+        cout<<"empty";
+    }
+}
+void isfull()
+{
+    if(top==10)
+    {
+        cout<<"overflow";
+
+    }
+
+}
+void push(int data)
+{   int data;
+    if(top<=9)
+    {
+        top++;
+        stack[top]=data;
+    }
+    else{
+        cout<<"stack is full";
+    }
+}
+int pop()
+{   int data;
+    if(top>=9)
+    {   
+        data=stack[top];
+        top--;
+    }
+    else{
+
+        cout<<"stack is empty";
+    }
+
+}
+int main()
+{
+
+        int data;
+        char ch;
+        cout<<"enter element : ";
+        cin>>data;
+        
+        do{
+             push(data);
+            cout<<"choose y for continue : ";
+                cin>>ch;
+                
+                cout<<"Enter element : ";
+                cin>>data;
+               
+
+        }
+        while(ch=='y' || ch=='Y');
+        
+      return 0;
+
+}
