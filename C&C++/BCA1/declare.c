fb10ce626b1d74328d841b22eba1dee3b6ab517c
nixis-institute <nixis.institute@gmail.com>
2018-12-26 10:52:03 +0530

    update

---
+++ C&C++/BCA1/declare.c
@@ -0,0 +1,13 @@
+//int 1, 2, 3
+//float 3.43, 5.32, 
+//char q, v,s  d, 
+
+#include<stdio.h>
+int main()
+{
+    float a,b,c;
+    a = 10.5;
+    b = 12.5;
+    c = a+b;
+    printf("%f and %f is %f",a,b,c);
+}
